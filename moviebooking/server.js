539058588c9d08e0bf5387c15b8eb445f9f1617f
kishore_kannan <kishorekannan63@gmail.com>
2021-07-15 21:39:47 +0530

    checkpoint5-Defined Schema and connected MongoDB

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -1,4 +1,18 @@
 const http=require('http');
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 const server=http.createServer((req,res)=>{
     if(req.url === '/movies'){
         res.write('All Movies Data in JSON format from Mongo DB');
