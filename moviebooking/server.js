efc56a0e6d09e2dbe0f9281dde1daccc4f3ec620
kishore_kannan <kishorekannan63@gmail.com>
2021-07-15 18:51:21 +0530

    checkpoint3-Add dynamic routes

---
+++ moviebooking/server.js
@@ -0,0 +1,17 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    if(req.url === '/movies'){
+        res.write('All Movies Data in JSON format from Mongo DB');
+        res.end();
+    }
+    if(req.url === '/genres'){
+        res.write('All Genres Data in JSON format from Mongo DB');
+        res.end();
+    }
+    if(req.url ==='/artists'){
+        res.write('All Artists Data in JSON format from Mongo DB');
+        res.end();
+    }
+})
+server.listen(9000,()=>console.log("listening on port 9000"));
+
